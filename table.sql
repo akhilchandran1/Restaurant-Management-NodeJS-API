4102af255c21520d5bbb22c09d9839d73aa613c0
Akhil Chandran <akhil9216@gmail.com>
2022-08-13 01:03:26 +0100

    Database connected

---
+++ table.sql
@@ -0,0 +1,15 @@
+
+-- To create table
+CREATE TABLE user (
+    id int primary key AUTO_INCREMENT,
+    name varchar(250),
+    contactNumber varchar(20),
+    email varchar(50),
+    password varchar(250),
+    status varchar(20),
+    role varchar(20),
+    UNIQUE (email)
+);
+
+-- To add user
+INSERT INTO user (name, contactNumber, email, password, status, role) VALUES ("admin", "123456789", "admin@gmail.com", "admin", "true", "admin");
