39ce041bdcd34cc30ddc9f5b285b1703abaae622
Akhil Chandran <akhil9216@gmail.com>
2022-08-16 23:42:18 +0100

    Forgot Password added

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,8 @@ const connection = require("../connection/connection");
 const router = express.Router();
 require("dotenv").config();
 const jsonwebtoken = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+const { response } = require("express");
 
 router.post("/signup", (req, res, next) => {
     let user = req.body;
@@ -47,6 +49,44 @@ router.post("/login", (req, res, next) => {
             return res.status(500).json(err);
         }
     });
+});
+
+var transporter = nodemailer.createTransport({
+    service: "gmail.com",
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    }
+});
+
+router.post("/forgotPassword", (req, res, next) => {
+    const user = req.body;
+    const query = "SELECT email, password FROM "+process.env.DB_TABLE+" WHERE email=?";
+    connection.query(query,[user.email], (err, results) => {
+        if(!err){
+            if(results.length <= 0){
+                console.log(results.length)
+                return res.status(200).json({message: "Password sent successfully to your email."});
+            }else{
+                var mailOptions = {
+                    from: process.env.EMAIL,
+                    to: results[0].email,
+                    subject: "Password by Restaurant management system",
+                    html: "<p><b>Your login details for your Restaurant management system<b><br><b>Email: </b>"+results[0].email+"<br><b>Password: </b>"+results[0].password+"<br><a href='http://localhost:4200/'>Click here to login</a></p>"
+                };
+                transporter.sendMail(mailOptions, function(error, info){
+                    if(error){
+                        console.log(error);
+                    }else{
+                        console.log("Email sent: "+info.response)
+                    }
+                });
+                return res.status(200).json({message: "Password sent successfully to your email."});
+            }
+        }else{
+            return res.status(500).json(err);
+        }
+    })
 });
 
 module.exports = router;
